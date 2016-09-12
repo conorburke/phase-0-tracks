a56c5de423a62bb9e9b2ab0e6f710943ca88fca1
conorburke <cjburke89@gmail.com>
2016-09-12 14:11:53 -0700

    Include booleans to check truthfulness

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,7 +6,26 @@ puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
+current_year = Time.new.year
+if (current_year - birth_year) == age
+  correct_age = true
+else
+  correct_age = false
+end
 puts "Should we order some garlic bread for you?"
-garlic_bread = gets.chomp
+garlic_bread = gets.chomp.downcase
+  if garlic_bread == "yes"
+    order_bread = true
+  else
+    order_bread = false
+  end
 puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+insurance = gets.chomp.downcase
+  if insurance == "yes"
+    buy_insurance = true
+  else
+    buy_insurance = false
+  end
+#check conditionals to determine if vampire 
+
+
