19f8e81a250570144e18f6e55b5b425cb0911e82
conorburke <cjburke89@gmail.com>
2016-09-12 14:03:01 -0700

    Ask basic questions to new employee

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+#challenge 4.4
+#vampire questionnaire
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+puts "Should we order some garlic bread for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
