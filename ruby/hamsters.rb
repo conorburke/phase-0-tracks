9687eb075b5cf0d4957af1dd96c9fb88c3c668df
conorburke <cjburke89@gmail.com>
2016-09-13 09:42:40 -0700

    Add basic hamster questions for user

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "What is the hamster's volume level?"
+name = gets.chomp
+puts "What is the fur color?"
+color = gets.chomp
+puts "Is the hamster a good or bad candidate for adoption?"
+candidate = gets.chomp
+puts "What is the hamster's age?"
+age = gets.chomp
+if age == ""
+  age = nil
+end
