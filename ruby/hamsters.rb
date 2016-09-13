2aa3e4ee8a58d2fb951187dca114301eb677384f
conorburke <cjburke89@gmail.com>
2016-09-13 09:52:21 -0700

    Add sentence at end to display data

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,7 +1,7 @@
 puts "What is the hamster's name?"
 name = gets.chomp
 puts "What is the hamster's volume level (1-10)?"
-name = gets.chomp.to_i
+volume = gets.chomp.to_i
 puts "What is the fur color?"
 color = gets.chomp
 puts "Is the hamster a good or bad candidate for adoption?"
@@ -10,4 +10,8 @@ puts "What is the hamster's age?"
 age = gets.chomp.to_i
 if age == ""
   age = nil
+  print_age = "unknown"
 end
+puts "The hamster's name is #{name} and has a volume of #{volume}.
+The fur color is #{color} and the hamster is a #{candidate} 
+candidate for adoption. The hamster's age is #{print_age}."
