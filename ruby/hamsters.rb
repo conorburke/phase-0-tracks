4646a09922aa3817036e052bc74deaf34d5409eb
conorburke <cjburke89@gmail.com>
2016-09-13 09:44:49 -0700

    Adjust answers to appropriate data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,13 @@
 puts "What is the hamster's name?"
 name = gets.chomp
-puts "What is the hamster's volume level?"
-name = gets.chomp
+puts "What is the hamster's volume level (1-10)?"
+name = gets.chomp.to_i
 puts "What is the fur color?"
 color = gets.chomp
 puts "Is the hamster a good or bad candidate for adoption?"
 candidate = gets.chomp
 puts "What is the hamster's age?"
-age = gets.chomp
+age = gets.chomp.to_i
 if age == ""
   age = nil
 end
