8195df09b65731322abc7d567a6130f3791a6fbe
conorburke <cjburke89@gmail.com>
2016-09-15 13:31:28 -0700

    Create encrypt method and test it

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -17,3 +17,21 @@
     change the array with the new letters back into a string
     print or return the string
 =end
+
+def encrypt(word)
+  counter = 0
+  code = []
+  while counter < word.length
+    if word[counter] == "z"
+      code[counter] = "a"
+    else
+      code[counter] = word[counter].next
+    end
+    counter += 1
+  end
+  code.join
+end
+
+def decrypt(wor)
+
+puts encrypt("zed")
