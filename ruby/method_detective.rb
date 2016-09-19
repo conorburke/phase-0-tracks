9917d730c07f4aa49e191439aeb336859f2bfd14
conorburke <cjburke89@gmail.com>
2016-09-19 16:54:17 -0700

    Finish method calls for required returns

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -26,18 +26,23 @@
 " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".delete("T")
+"The mystery of the missing first letter".delete("t".upcase)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".gsub(/\s+/, " ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# This is the ASCII / Unicode designator for lower-case "z". Useful for 
+# manipulating strings such as the encryption release in week 4
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
+"How many times does the letter 'a' appear in this string?".count("A".downcase)
 # => 4
