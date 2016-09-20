f196b6bfe78ec2fe6fb2c4ac0517ce19d5269881
conorburke <cjburke89@gmail.com>
2016-09-20 12:23:09 -0700

    Initialize with nds of nfl teams

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,56 @@
+#nested data structure of the nfl
+
+nfl = {
+  national: {
+    east: [
+      'Dallas',
+      'New York',
+      'Philadelphia',
+      'Washington'
+    ]
+    north: [
+      'Chicago',
+      'Detroit',
+      'Green Bay',
+      'Minnesota'
+    ]
+    south: [
+      'Atlanta',
+      'Carolina',
+      'New Orleans',
+      'Tampa Bay'
+    ]
+    west: [
+      'Arizona',
+      'Los Angeles',
+      'San Francisco',
+      'Seattle'
+    ]
+  } 
+  american: {
+    east: [
+      'Buffalo',
+      'Miama',
+      'New England',
+      'New York'
+    ]
+    north: [
+      'Baltimore',
+      'Cleveland',
+      'Cincinnati',
+      'Pittsburgh'
+    ]
+    south: [
+      'Houston',
+      'Indianapolis',
+      'Jacksonville',
+      'Tennessee',
+    ]
+    west: [
+      'Denver',
+      'Kansas City',
+      'Oakland',
+      'San Diego'
+    ]
+  }
+}
