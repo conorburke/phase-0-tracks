f785085d85af2ee3f87dad0e98c0ecbbdae2ef72
conorburke <cjburke89@gmail.com>
2016-09-20 12:52:02 -0700

    Add methods to manipulate data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -7,50 +7,60 @@ nfl = {
       'New York',
       'Philadelphia',
       'Washington'
-    ]
+    ],
     north: [
       'Chicago',
       'Detroit',
       'Green Bay',
       'Minnesota'
-    ]
+    ],
     south: [
       'Atlanta',
       'Carolina',
       'New Orleans',
       'Tampa Bay'
-    ]
+    ],
     west: [
       'Arizona',
-      'Los Angeles',
+      'St.Louis',
       'San Francisco',
       'Seattle'
     ]
-  } 
+  }, 
   american: {
     east: [
       'Buffalo',
       'Miama',
       'New England',
       'New York'
-    ]
+    ],
     north: [
       'Baltimore',
       'Cleveland',
       'Cincinnati',
-      'Pittsburgh'
-    ]
+      'hgrubsttiP'
+    ],
     south: [
       'Houston',
       'Indianapolis',
       'Jacksonville',
       'Tennessee',
-    ]
+    ],
     west: [
       'Denver',
       'Kansas City',
       'Oakland',
-      'San Diego'
     ]
   }
 }
+
+nfl[:american][:west].push('San Diego')
+nfl[:national][:west][1] = "Los Angeles"
+nfl[:american][:north][3].reverse!
+nfl[:national][:london] = []
+#nfl_teams = nfl[:national][:east].map do |key, new_team|
+#  team_names = ['Cowboys', 'Giants', 'Eagles', 'Redskins']
+#  :east[key] = team_names[new_team]
+#end
+p nfl
+#p nfl_teams
