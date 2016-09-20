6b28d5e261190c250e86bc7d788e8c72940620ad
conorburke <cjburke89@gmail.com>
2016-09-20 16:12:21 -0700

    Add loop for multiple names to be added

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,79 +1,89 @@
-puts "What is your first name?"
-first_name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
-full_name = first_name + " " + last_name
-full_name_arr = full_name.split
-first_name_arr = full_name_arr.shift
-full_name_arr.push(first_name_arr)
-full_name = full_name_arr.join(' ')
+puts "Welcome to the alias name generator."
+continue = true
+while continue
+  puts "What is your first name?"
+  first_name = gets.chomp
+  puts "What is your last name?"
+  last_name = gets.chomp
+  full_name = first_name + " " + last_name
+  full_name_arr = full_name.split
+  first_name_arr = full_name_arr.shift
+  full_name_arr.push(first_name_arr)
+  full_name = full_name_arr.join(' ')
 
-spy_array = []
+  spy_array = []
 
-full_name_arr = full_name.chars
-p full_name_arr
-full_name_arr.map! do |letter|
-  if letter.ord == 32
-    spy_array << " "
-  elsif letter.ord < 91
-    case letter.ord
-    when 65
-      spy_array << "E"
-    when 68
-      spy_array << "F"
-    when 69
-      spy_array << "I"
-    when 72
-      spy_array << "J"
-    when 73
-      spy_array << "O"
-    when 78
-      spy_array << "P"
-    when 79
-      spy_array << "U"
-    when 84
-      spy_array << "V"
-    when 85
-      spy_array << "A"
-    when 90
-      spy_array << "B"
-  else
-    spy_array << (letter.ord + 1).chr
-  end
-  elsif letter.ord < 123
-    case letter.ord
-    when 97
-      spy_array << "e"
-    when 100
-      spy_array << "f"
-    when 101
-      spy_array << "i"
-    when 104
-      spy_array << "j"
-    when 105
-      spy_array << "o"
-    when 110
-      spy_array << "p"
-    when 111
-      spy_array << "u"
-    when 116
-      spy_array << "v"
-    when 117
-      spy_array << "a"
-    when 122
-      spy_array << "B"
+  full_name_arr = full_name.chars
+  #p full_name_arr
+  full_name_arr.map! do |letter|
+    if letter.ord == 32
+      spy_array << " "
+    elsif letter.ord < 91
+      case letter.ord
+      when 65
+        spy_array << "E"
+      when 68
+        spy_array << "F"
+      when 69
+        spy_array << "I"
+      when 72
+        spy_array << "J"
+      when 73
+        spy_array << "O"
+      when 78
+        spy_array << "P"
+      when 79
+        spy_array << "U"
+      when 84
+        spy_array << "V"
+      when 85
+        spy_array << "A"
+      when 90
+        spy_array << "B"
     else
       spy_array << (letter.ord + 1).chr
-    end      
-  else
-    spy_array << "*"
+    end
+    elsif letter.ord < 123
+      case letter.ord
+      when 97
+        spy_array << "e"
+      when 100
+        spy_array << "f"
+      when 101
+        spy_array << "i"
+      when 104
+        spy_array << "j"
+      when 105
+        spy_array << "o"
+      when 110
+        spy_array << "p"
+      when 111
+        spy_array << "u"
+      when 116
+        spy_array << "v"
+      when 117
+        spy_array << "a"
+      when 122
+        spy_array << "B"
+      else
+        spy_array << (letter.ord + 1).chr
+      end      
+    else
+      spy_array << "*"
+    end
   end
+  #p spy_array
+  spy_name = spy_array.join
+  #p spy_name
+  puts "If you would like to quit, type 'quit'. Otherwise,"\
+  "just hit enter to enter a new name!"
+  answer = gets.chomp.to_s.downcase
+    if answer == "quit"
+      continue = false
+    else
+      continue == true
+    end
 end
-
-p spy_array
-spy_name = spy_array.join
-p spy_name
-
 
 
 
