44fba04a6f1462e7416d885b3f346b5fa81bd8ee
conorburke <cjburke89@gmail.com>
2016-09-20 10:04:02 -0700

    Iterate through .each and .map for array and .each for hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,16 @@ end
 print_status do |middle|
   puts "This is the #{middle} status."  
 end
+#add array and hash and use .each and .map on them
+animals = ["camel","elephant","tiger"]
+football_team = { seattle: "seahawks", cleveland: "browns", baltimore: "ravens" }
+animals.each do |animal|
+  puts "the name of the animal is #{animal}."
+end
+football_team.each do |city, team|
+  puts "The #{team} are from #{city}."
+end
+animals.map! do |animal|
+  animal.reverse
+end
+puts animals
