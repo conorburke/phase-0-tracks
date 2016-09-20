bee48c1c91e32de34271e09050acaf2792ba6c28
conorburke <cjburke89@gmail.com>
2016-09-19 22:02:32 -0700

    Impliment driver code with conditions and info check

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,6 +13,7 @@ Pseudocode a program that puts designer input into a hash
 -exit program
 =end
 
+#initialize client hash
 client = {
   name: "",
   age: 0,
@@ -23,6 +24,7 @@ client = {
   area: "",
 }
 
+#prompt designer for client info
 puts "What is the name of the client?"
 client[:name] = gets.chomp.to_s
 puts "What is the age of the client?"
@@ -48,4 +50,44 @@ end
 puts "What is the city or area the client will be in?"
 client[:area] = gets.chomp.to_s
 
+#check with designer that everything is correct
+puts "Here is the client info:"
 puts client
+puts "Are there any mistakes? Please specify which category has"\
+"a mistake or simply write 'none'."
+error_check = gets.chomp
+if error_check == "none"
+  puts "Great!  Here is your client info:"
+  puts client
+elsif error_check == "name" || error_check == "decor_theme" || error_check == "area"
+  puts "What is the correct info?"
+  corrected = gets.chomp.to_s
+  client[error_check.to_sym] = corrected
+  puts "And here is the corrected info:"
+  puts client
+elsif error_check == "age" || error_check == "num_children"
+  puts "What is the correct info?"
+  corrected = gets.chomp.to_i
+  client[error_check.to_sym] = corrected
+  puts "And here is the corrected info:"
+  puts client
+elsif error_check == "married"
+  if client[:married] == true
+    client[:married] = false
+  else
+    client[:married] = true
+  end
+  puts "And here is the corrected info:"
+  puts client  
+elsif error_check == "rural"
+  if client[:rural] == true
+    client[:rural] = false
+  else
+    client[:rural] = true
+  end
+  puts "And here is the corrected info:"
+  puts client
+else
+  puts "That was not a type of info.  Here is the client info:"
+  puts client
+end
