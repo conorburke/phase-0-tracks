57b3532c4209f14e7c996b103fecf206a87fe083
conorburke <cjburke89@gmail.com>
2016-09-20 16:00:19 -0700

    Change all lowercase to next consonant or vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -19,29 +19,54 @@ full_name_arr.map! do |letter|
     spy_array << " "
   elsif letter.ord < 91
     case letter.ord
-      when 65
-        spy_array << "E"
-      when 68
-        spy_array << "F"
-      when 69
-        spy_array << "I"
-      when 72
-        spy_array << "J"
-      when 73
-        spy_array << "O"
-      when 78
-        spy_array << "P"
-      when 79
-        spy_array << "U"
-      when 84
-        spy_array << "V"
-      when 85
-        spy_array << "A"
-      when 90
-        spy_array << "B"
+    when 65
+      spy_array << "E"
+    when 68
+      spy_array << "F"
+    when 69
+      spy_array << "I"
+    when 72
+      spy_array << "J"
+    when 73
+      spy_array << "O"
+    when 78
+      spy_array << "P"
+    when 79
+      spy_array << "U"
+    when 84
+      spy_array << "V"
+    when 85
+      spy_array << "A"
+    when 90
+      spy_array << "B"
+  else
+    spy_array << (letter.ord + 1).chr
+  end
+  elsif letter.ord < 123
+    case letter.ord
+    when 97
+      spy_array << "e"
+    when 100
+      spy_array << "f"
+    when 101
+      spy_array << "i"
+    when 104
+      spy_array << "j"
+    when 105
+      spy_array << "o"
+    when 110
+      spy_array << "p"
+    when 111
+      spy_array << "u"
+    when 116
+      spy_array << "v"
+    when 117
+      spy_array << "a"
+    when 122
+      spy_array << "B"
     else
       spy_array << (letter.ord + 1).chr
-    end 
+    end      
   else
     spy_array << "*"
   end
