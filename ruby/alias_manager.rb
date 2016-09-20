23c2e96f6a286ca9cb406deb893751c840d966ce
conorburke <cjburke89@gmail.com>
2016-09-20 14:43:52 -0700

    Add pseudocode to file for steps to be taken

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+=begin 
+Pseudocode the solo challenge for flipping names and forwarding
+letters
+  -ask user for first name
+    -store as string
+  -ask user for last name
+    -store as string
+  -concatenate strings
+  -use method to turn string into array of two words
+    -use shift and push to reorder array
+  -iterate through array of characters to identify them 
+    -use reference arrays, next, and ord/chr to adust letters
+  -adjust for edge cases
+  -change type back into string
+  -set up loop for multiple iterations of this process
+  -print out results at end
+=end
