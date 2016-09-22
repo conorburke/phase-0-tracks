15956ee324b4ed882765772948a0cd3508ff23e2
conorburke <cjburke89@gmail.com>
2016-09-21 23:42:54 -0700

    Seperate code into additional character changer method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,19 +1,8 @@
-def alias_maker(first_name, last_name)
-  #turn into full name string
-  full_name = first_name + " " + last_name
-  #turn name into array and take out first name
-  full_name_arr = full_name.split
-  first_name_arr = full_name_arr.shift
-  #add full name to end and turn back into string
-  full_name_arr.push(first_name_arr)
-  full_name = full_name_arr.join(' ')
-  #create empty array to append new values to
-  spy_array = []
-  #turn string back into array to iterate through each char
-  full_name_arr = full_name.chars
+def character_changer(characters)
   #iterate through and save new character to spy_array
   #use ASCII values to catch all cases
-  p full_name_arr
+  #create empty array to append new values to
+  spy_array = []
   exceptions = {
   32 => " ",
   65 => "E",
@@ -37,98 +26,30 @@ def alias_maker(first_name, last_name)
   117 => "a",
   122 => "b"
 }
-#exceptions_index = [32,65,68,69,72,73,78,79,84,85,90,97,100,
-#  101,104,105,110,111,116,117,122]
-
-  full_name_arr.each do |letter|
-    
+  characters.each do |letter|
     if exceptions.has_key?(letter.ord)
       spy_array << exceptions[letter.ord]
     else
       spy_array << (letter.ord + 1).chr
     end
   end
-=begin
-exceptions2 = {
-  " " => 32,
-  "E" => 65,
-  "F" => 68,
-  "I" => 69,
-  "J" => 72,
-  "O" => 73,
-  "P" => 78,
-  "U" => 79,
-  "V" => 84,
-  "A" => 85,
-  "B" => 90,
-  "e" => 97,
-  "f" => 100,
-  "i" => 101,
-  "j" => 104,
-  "o" => 105,
-  "p" => 110,
-  "u" => 111,
-  "v" => 116,
-  "a" => 117,
-  "b" => 122
-}
-exceptions3 = {
-  " " => " ",
-  "A" => "E",
-  "D" => "F",
-  "E" => "I",
-  "H" => "J",
-  "I" => "O",
-  "N" => "P",
-  "O" => "U",
-  "T" => "V",
-  "U" => "A",
-  "Z" => "B",
-  "a" => "e",
-  "d" => "f",
-  "e" => "i",
-  "h" => "j",
-  "i" => "o",
-  "n" => "p",
-  "o" => "u",
-  "t" => "v",
-  "u" => "a",
-  "z" => "b"
-}
-=end
-
-
-=begin
-exceptions_index.each do |check|
-  counter = 0
-  if check == full_name_arr[counter].ord
-    full_name_arr[counter] = exceptions[check]
-  end
-  counter += 1
-end
-=end
-=begin
-  full_name_arr.each do |letter|
-    counter = 0
-    while counter < exceptions_index.length
-      if exceptions_index[counter] == letter.ord
-        spy_array << exceptions[exceptions_index[counter]]
-        p spy_array
-        counter += 1
-        break
-      else
-        spy_array << (letter.ord + 1).chr
-        p spy_array 
-        break
-      end
-      counter += 1    
-    end
-  end
-=end
   #turn array into string
   spy_name = spy_array.join
 end
 
+def alias_maker(first_name, last_name)
+  #turn into full name string
+  full_name = first_name + " " + last_name
+  #turn name into array and take out first name
+  full_name_arr = full_name.split
+  first_name_arr = full_name_arr.shift
+  #add full name to end and turn back into string
+  full_name_arr.push(first_name_arr)
+  full_name = full_name_arr.join(' ')
+  #turn string back into array to iterate through each char
+  full_name_arr = full_name.chars
+  character_changer(full_name_arr)
+end
 
 
 puts "Welcome to the alias name generator."
