bc5cd148cc214ed1d5718344d635617ff0ac81d6
conorburke <cjburke89@gmail.com>
2016-09-21 21:37:23 -0700

    Change case statements to hashes with exception values

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,73 +13,49 @@ def alias_maker(first_name, last_name)
   full_name_arr = full_name.chars
   #iterate through and save new character to spy_array
   #use ASCII values to catch all cases
+  uppercase_exceptions = {
+  65 => "E",
+  68 => "F",
+  69 => "I",
+  72 => "J",
+  73 => "O",
+  78 => "P",
+  79 => "U",
+  84 => "V",
+  85 => "A",
+  90 => "B",
+}
+
+lowercase_exceptions = {
+  97 => "e",
+  100 => "f",
+  101 => "i",
+  104 => "j",
+  105 => "o",
+  110 => "p",
+  111 => "u",
+  116 => "v",
+  117 => "a",
+  122 => "b",
+}
   full_name_arr.each do |letter|
     #for space character
     if letter.ord == 32
       spy_array << " "
     #for uppercase characters  
     elsif letter.ord < 91
-      case letter.ord
-      when 65
-        spy_array << "E"
-      when 68
-        spy_array << "F"
-      when 69
-        spy_array << "I"
-      when 72
-        spy_array << "J"
-      when 73
-        spy_array << "O"
-      when 78
-        spy_array << "P"
-      when 79
-        spy_array << "U"
-      when 84
-        spy_array << "V"
-      when 85
-        spy_array << "A"
-      when 90
-        spy_array << "B"
-      else
-      spy_array << (letter.ord + 1).chr
-      end
+      spy_array << uppercase_exceptions[letter.ord]
     #for lowercase characters
     elsif letter.ord < 123
-      case letter.ord
-      when 97
-        spy_array << "e"
-      when 100
-        spy_array << "f"
-      when 101
-        spy_array << "i"
-      when 104
-        spy_array << "j"
-      when 105
-        spy_array << "o"
-      when 110
-        spy_array << "p"
-      when 111
-        spy_array << "u"
-      when 116
-        spy_array << "v"
-      when 117
-        spy_array << "a"
-      when 122
-        spy_array << "B"
-      else
-        spy_array << (letter.ord + 1).chr
-      end      
+      spy_array << lowercase_exceptions[letter.ord]
     else
-      spy_array << "*"
+        spy_array << (letter.ord + 1).chr      
     end
   end
   #turn array into string
   spy_name = spy_array.join
 end
 
-uppercase = {
-  
-}
 
 
 puts "Welcome to the alias name generator."
