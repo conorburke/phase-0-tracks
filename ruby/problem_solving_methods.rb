8d1a816edf6a04caf1ee42ad1e1ea5d69e5ab01b
conorburke <cjburke89@gmail.com>
2016-09-22 09:17:49 -0700

    Create fibonacci method to list specified numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+=begin
 numbers = [1,2,3,4,5]
 
 def search_number(arr, num)
@@ -11,3 +12,17 @@ def search_number(arr, num)
 end
 
 p search_number(numbers, 6)
+=end
+
+def fibonacci(num)
+  sequence = [0,1]
+  counter = 2
+  while counter < num 
+    sequence[counter] = sequence[counter - 1] + 
+      sequence[counter - 2]
+    counter += 1
+  end
+  sequence
+end
+
+p fibonacci(6)
