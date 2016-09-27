48c116354140dc287321a2b07b2168467298cf66
conorburke <cjburke89@gmail.com>
2016-09-27 12:41:40 -0700

    Add initialize method to class Puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,12 +1,14 @@
 class Puppy
-
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
   def speak(time)
     time.times do |time|
-    puts "Woof!"
+      puts "Woof!"
     end 
   end
   def roll_over 
