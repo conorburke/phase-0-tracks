d865b61762da2e83426a787125207cda98d0ccd0
conorburke <cjburke89@gmail.com>
2016-09-26 20:58:05 -0700

    Create class outline for Puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,28 @@
+=begin
+
+Brainstorm characteristics of a puppy class
+
+Species (Class): Puppy (could be a subclass of Dog)
+
+CHARACTERISTICS ----------------
+Name: varies
+Breed: varies
+Age: varies
+Color: varies
+Gender: male or female
+Eye count: 2
+Leg Count: 4 (most cases, unless dog injured)
+House-trained: yes or no
+
+BEHAVIOR -----------------------
+Jump
+Sit
+Lay down
+Roll over
+Fetch
+Eat
+Go to bathroom
+Sleep
+Lick
+
+=end
