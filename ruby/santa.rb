373597bf1c6953b04a533f2ce8b194dae163b583
conorburke <cjburke89@gmail.com>
2016-09-28 17:25:41 -0700

    Create 1000000 instances of Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-  attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_reader :ethnicity
+  attr_accessor :age, :gender
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -18,10 +18,10 @@ class Santa
   def celebrate_birthday
     @age += 1
   end
-  def get_mad_at(reindeer)
-    @reindeer_ranking.delete(reindeer)
-    @reindeer_ranking.push(reindeer)
-  end
+  # def get_mad_at(reindeer)
+  #   @reindeer_ranking.delete(reindeer)
+  #   @reindeer_ranking.push(reindeer)
+  #end
   # def gender=(new_gender)
   #   @gender = new_gender
   # end
@@ -33,19 +33,34 @@ class Santa
   # end
 end
 
-santas = []
+santa_count = 1000000
 genders = ["female", "male", "bigender", "agender", "fluid"]
 ethnicities = ["black", "white", "latino", "asian", "other"]
 
-genders.each_index do |counter|
-  santas.push(Santa.new(genders[counter], ethnicities[counter]))
+santa_count.times do |counter|
+  current_santa = Santa.new(genders.sample, ethnicities.sample)
+  current_santa.age = rand(141)
+  puts "This santa is #{current_santa.age} years old. "\
+  "The gender is #{current_santa.gender} and the ethnicity "\
+  "is #{current_santa.ethnicity}."
+  # puts current_santa.age
+  # puts current_santa.gender
+  # puts current_santa.ethnicity
 end
-p santas
-santas[0].speak
-santas[0].eat_milk_and_cookies("sugar")
-santas[0].celebrate_birthday
-santas[0].get_mad_at("Dancer")
-santas[0].gender = "male"
-p santas
-p santas[0].age
-p santas[0].ethnicity
+
+# santas = []
+# genders = ["female", "male", "bigender", "agender", "fluid"]
+# ethnicities = ["black", "white", "latino", "asian", "other"]
+
+# genders.each_index do |counter|
+#   santas.push(Santa.new(genders[counter], ethnicities[counter]))
+# end
+# p santas
+# santas[0].speak
+# santas[0].eat_milk_and_cookies("sugar")
+# santas[0].celebrate_birthday
+# santas[0].get_mad_at("Dancer")
+# santas[0].gender = "male"
+# p santas
+# p santas[0].age
+# p santas[0].ethnicity
