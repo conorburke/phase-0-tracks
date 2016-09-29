c30957cd6f208accde4e099173a350a3ef53caff
conorburke <cjburke89@gmail.com>
2016-09-28 20:36:25 -0700

    Create two classes and include shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,30 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happilly(words)
+#     words + "!!!" + " ;)"
+#   end
+# end
+
+# puts Shout.yell_angrily("Go away")
+# puts Shout.yell_happilly("Come here")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yell_happilly(words)
-    words + "!!!" + " ;)"
+  def yell_enthusiastically(team)
+    "The " + team + " are the best team in the league!"
   end
 end
 
-puts Shout.yell_angrily("Go away")
-puts Shout.yell_happilly("Come here")
+class EaglesFan
+  include Shout
+end
+
+class CowboysFan
+  include Shout
+end
+
+eagles = EaglesFan.new
+cowboys = CowboysFan.new
+puts eagles.yell_enthusiastically("Eagles")
+puts cowboys.yell_enthusiastically("Cowboys")
