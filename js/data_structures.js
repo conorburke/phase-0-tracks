cad0c9a3c835cad7aad39c617218a77b18ab276d
conorburke <cjburke89@gmail.com>
2016-10-04 09:19:08 -0700

    Create a stable object to store horse and color attributes

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,10 @@ horses.push("Lenny");
 
 console.log(colors);
 console.log(horses);
+var stable = {};
+if (colors.length === horses.length){
+  for (i=0; i<colors.length; i++){
+    stable[horses[i]] = colors[i];
+  }
+}
+console.log(stable);
