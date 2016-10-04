3b7f1fb59d53d9fc8328cd01697024a15d56a6e5
conorburke <cjburke89@gmail.com>
2016-10-04 09:06:22 -0700

    Create color and horse array and append one to each

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "green", "pink", "black"];
+var horses = ["Gallop", "Jumper", "Secretariat", "George"];
+
+colors.push("red");
+horses.push("Lenny");
+
+console.log(colors);
+console.log(horses);
