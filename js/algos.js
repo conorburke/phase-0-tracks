77ba30e81e23e1cd2122355d4cddfa72c7c54a92
conorburke <cjburke89@gmail.com>
2016-10-05 17:58:34 -0700

    Pseudocode solution for longest string in array

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+/*
+  pseudocode function for longest word in array
+    -create array with several strings stored in it
+    -define function that takes one parameter (an array)
+    -in function, loop through the strings in the array
+      -find and store the length of each string
+      -keep track of which one is the longest and store that in 
+        a separate variable
+      -return the variable that has the longest string stored in it
+*/
+
+
