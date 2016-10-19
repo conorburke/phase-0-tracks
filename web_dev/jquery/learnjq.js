fd7364ba04b31b7cf1e06c99a070103f16d9e7b2
conorburke <cjburke89@gmail.com>
2016-10-18 18:26:14 -0700

    Create jquery effects on html page

--- web_dev/jquery/learnjq.js
+++ web_dev/jquery/learnjq.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
 
   $('#lizard-photo').slideUp(500).slideDown(500);
 
+  $('button').hover(function(){
+    $(this).fadeOut(500).fadeIn(1500);
+  });
 
-
-
-
+  $('button').click(function(){
+    $(this).css('background-color', 'red');
+  });
 
 });
